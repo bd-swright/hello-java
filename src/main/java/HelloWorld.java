ad4d72cef20a8830749cbb617503d7ba94152941
Shane Wright <swright@blackduck.com>
2026-05-12 15:52:20 -0400

    risky crypto

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,6 +3,9 @@ public class HelloWorld {
         String secret = "It's a secret to everybody.";
         try { javax.crypto.spec.SecretKeySpec keyspec = new javax.crypto.spec.SecretKeySpec(secret.getBytes("UTF-8"), "AES"); }
         catch (Exception e) { System.out.println("Something went wrong."); }
+        // INTENTIONALLY WEAK CIPHER - SAST fixture for CWE-327 (broken/risky crypto). Do not use DES in real code.
+        try { javax.crypto.Cipher weak = javax.crypto.Cipher.getInstance("DES"); }
+        catch (Exception e) { System.out.println("Something went wrong."); }
         System.out.println("Hello World!");
         while (true) {
             System.out.println("Are we there yet?");
